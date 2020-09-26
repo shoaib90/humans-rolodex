ec33163b11262e7cfa41c4ef71226078b7e3f62a
Quest <shoaibkhan3412@gmail.com>
2020-09-26 20:11:28 +0530

    Updates

---
+++ precache-manifest.8d204c1f6c81bfecd655221063acf322.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd5062836d66f5959b9d20567f97da17",
+    "url": "/humans-rolodex/index.html"
+  },
+  {
+    "revision": "5c68692d6a3e8530d13a",
+    "url": "/humans-rolodex/static/css/main.9739ebcf.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/humans-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/humans-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c68692d6a3e8530d13a",
+    "url": "/humans-rolodex/static/js/main.00a34486.chunk.js"
+  },
+  {
+    "revision": "8cb900f8a124ad73f00a",
+    "url": "/humans-rolodex/static/js/runtime-main.bc021e28.js"
+  }
+]);
